35d1f1ce0edcc4b757f2edef9a36a12eaaad437e
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-06-30 23:55:50 -0500

    hashing the password, not working yet

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
-
 from flaskblog.models import User, Post
 
 posts = [
@@ -39,8 +38,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(message=f"Account created for {form.username.data}!", category='success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(message="Your account has been created. You are ready to log in not.", category='success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['POST', 'GET'])
