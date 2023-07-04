7b9ecc2c0caaaebd2cf8b7daa3f193c5d9389aa4
Krisztian Markella <chris.krisztian.markella@gmail.com>
2023-07-04 13:54:56 -0500

    username and email update implemented

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -23,6 +24,27 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError(message='That email is taken. Please chose a different one.')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(label='Username',
+                           validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField(label='Email',
+                           validators=[DataRequired(), Email()])
+
+    submit = SubmitField(label='Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(message='That user is taken. Please chose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(message='That email is taken. Please chose a different one.')
 
 
 class LoginForm(FlaskForm):
